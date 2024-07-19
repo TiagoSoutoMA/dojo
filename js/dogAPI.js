3fd99870c280ddbeba2de4d57799ba5fe30eac11
Giovani Fogarolli <fogarolligiovani@gmail.com>
2024-07-19 16:13:26 -0300

    Adição começo do fetch de imagem

--- js/dogAPI.js
+++ js/dogAPI.js
@@ -1,6 +1,21 @@
 document.addEventListener('DOMContentLoaded', async() => {
     const response = await fetch("https://dog.ceo/api/breeds/list/all")
     .then(response => response.json())
-    .then(data => {data.message; console.log(data.message)})
-    
+    .then(data => {
+        const race = Object.keys(data.message);
+        const raceRandom = Math.floor(Math.random() * race.length);
+        fetchDogIMg(race[raceRandom]);
+    })
 })
+
+function fetchDogIMg(name){
+    try{
+        fetch(`https://dog.ceo/api/breed/${name}/images/random`)
+            .then(response => response.json())
+            .then(data => {
+                
+            })
+    }catch(error){
+        console.error("Erro" + error)
+    }
+}
