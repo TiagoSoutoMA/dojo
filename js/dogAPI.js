7c2745a11144c3c1b433bb9f620366c69882dbe6
PauloPeixotoo <paulopeixotoo@hotmail.com>
2024-07-19 16:32:50 -0300

    feat:adiciona nome dog

--- js/dogAPI.js
+++ js/dogAPI.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       const DOG_QNT = 10;
 
       for (let i = 0; i < DOG_QNT; i++) {
-        fetchDogIMg(race[i]);
+        fetchDogIMg(race[Math.floor(Math.random() * race.length)]);
       }
     });
 });
@@ -16,11 +16,14 @@ function fetchDogIMg(name) {
     fetch(`https://dog.ceo/api/breed/${name}/images/random`)
       .then((response) => response.json())
       .then((data) => {
+        const nameDog = document.createElement("p");
+        nameDog.innerText = name;
         const img = document.createElement("img");
         img.src = data.message;
         const dogContainer = document
           .getElementById("dogContainer")
-          .appendChild(img);
+          dogContainer.appendChild(img );
+          dogContainer.appendChild(nameDog)
       });
   } catch (error) {
     console.error("Erro" + error);
