7d612f7eeec62acef743cd24d54f8609993e95c3
Davi Nunes <davi.nunes05@gmail.com>
2024-07-19 15:28:32 -0300

    acerto

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("dom-content-load", async () => {
+document.addEventListener("DOMContentLoaded", async () => {
 	const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10")
 		.then((response) => response.json())
 		.then((data) => {
